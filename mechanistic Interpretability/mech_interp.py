117f81a72cbca07cdcfcdf6caa3176ab84857518
raman1383 <raman83shariati@gmail.com>
2026-06-06 17:20:38 -0800

    5th

---
+++ mechanistic Interpretability/mech_interp.py
@@ -0,0 +1,18 @@
+"""
+Mechanistic Interpretability(MechInterp)
+
+tries to understand the inner workings AI models, 
+more you understand, better you can build.
+
+
+
+
+
+randomized labels memorization experiment(rethinking gerenralization) -> deep models even with regularization 
+are perfectly capable of memorizing random labels, which suggests that the capacity of deep neural networks 
+is much higher than previously thought
+
+double descent, bias-variance tradeoff, lessons from modulo addition
+
+inception V1
+"""
